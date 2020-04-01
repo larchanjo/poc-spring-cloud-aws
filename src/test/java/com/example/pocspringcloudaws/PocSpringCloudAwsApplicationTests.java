b91c3b9cc254d60ca2c292de2fe384f8a4179fc0
Luram Archanjo <luram.archanjo@sensedia.com>
2020-04-01 14:11:21 -0300

    Initial Project

---
+++ src/test/java/com/example/pocspringcloudaws/PocSpringCloudAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocspringcloudaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringCloudAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
